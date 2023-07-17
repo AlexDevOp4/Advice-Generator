f62c025c53eef41d6863b7a6c4c052cc9ca5a441
Alex <alex.ashtiany@icloud.com>
2023-07-17 08:13:26 -0400

    upading

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        drkGreyBlue: "#313A48",
+        greyBlue: "#4F5D74",
+        neonGreen: "#53FFAA",
+        lightCyan: "#CEE3E9",
+      },
+      inset: {
+        "165px": "165px",
+      },
+    },
   },
   plugins: [],
-}
+};
